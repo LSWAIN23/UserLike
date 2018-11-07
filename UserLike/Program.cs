e7910cbf4355e2ee72e558777c180d60828bc99c
hackercentral <levi.swain1@outlook.com>
2018-11-06 19:14:44 -0500

    initial commit

---
+++ UserLike/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program that prompts the user asking who they are and what they like; then print the statements back to the user with their input
+
+namespace UserLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is something you like to do?");
+            string hobby = Console.ReadLine();
+            Console.WriteLine(Hobby(name, hobby));
+
+        }
+
+        static public string Hobby(string name, string hobby)
+        {
+            Console.WriteLine($"Hello, you are {name}, and you like to {hobby}");
+            return null;
+        }
+    }
+}
